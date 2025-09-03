e755eea7d61f76f5471f4e1f4390b8e09cf9b18e
Manideep Shivanathula <mshivanathula@worksoft.com>
2025-09-03 18:06:54 +0530

    three

---
+++ student lists/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student_lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> students = new List<string>();
+
+            students.Add("Rahul");
+            students.Add("Sneha");
+            students.Add("Amit");
+            students.Add("Priya");
+
+            Console.WriteLine("Students are:");
+            foreach (string s in students)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
