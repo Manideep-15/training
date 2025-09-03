84f014e526ea57ab41bda3ad8ca5545978b7d619
Manideep Shivanathula <mshivanathula@worksoft.com>
2025-09-03 18:24:47 +0530

    6

---
+++ index of elements/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace index_of_elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> fruits = new List<string>() { "apple", "banana", "mango", "grapes" };
+
+            Console.WriteLine("Enter fruit name to search:");
+            string name = Console.ReadLine();
+
+            int index = fruits.IndexOf(name);
+
+            if (index != -1)
+            {
+                Console.WriteLine(name + " found at index " + index);
+            }
+            else
+            {
+                Console.WriteLine("Not found");
+            }
+        }
+    }
+}
