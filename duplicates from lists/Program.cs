174d921b26123e88a94345441d9aba7fa76df222
Manideep Shivanathula <mshivanathula@worksoft.com>
2025-09-03 18:20:23 +0530

    5

---
+++ duplicates from lists/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace duplicates_from_lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = new List<int>() { 1, 2, 2, 3, 4, 4, 5 };
+            HashSet<int> uniqueNums = new HashSet<int>(nums);
+
+            Console.WriteLine("Unique numbers:");
+            foreach (int n in uniqueNums)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
