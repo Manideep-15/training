37af4785933233af3cc51b2a4d88f9464e329f05
Manideep Shivanathula <mshivanathula@worksoft.com>
2025-09-03 17:53:22 +0530

    onee

---
+++ palindrome/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "madam";
+            string reversed = "";
+
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                reversed += text[i];
+            }
+
+            if (reversed == text)
+            {
+                Console.WriteLine("It is palindrome");
+            }
+            else
+            {
+                Console.WriteLine("It is not a palindrome");
+            }
+        }
+    }
+}
