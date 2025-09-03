fe6dd8772f6b88856b09dbc52cca328e560fb2a4
Manideep Shivanathula <mshivanathula@worksoft.com>
2025-09-03 18:28:17 +0530

    7

---
+++ reverse words in sentence/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverse_words_in_sentence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "C# is fun to learn";
+            string[] parts = sentence.Split(' ');
+
+            for (int i = parts.Length - 1; i >= 0; i--)
+            {
+                Console.Write(parts[i] + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
