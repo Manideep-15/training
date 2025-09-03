9bc3d4fa4b11c3b1a279b44d5781f01631f545d2
Manideep Shivanathula <mshivanathula@worksoft.com>
2025-09-03 18:08:57 +0530

    four

---
+++ word frequency using dictionary/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace word_frequency_using_dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "hello world hello code";
+            string[] words = sentence.Split(' ');
+
+            Dictionary<string, int> freq = new Dictionary<string, int>();
+
+            foreach (string w in words)
+            {
+                if (freq.ContainsKey(w))
+                {
+                    freq[w]++;
+                }
+                else
+                {
+                    freq[w] = 1;
+                }
+            }
+
+            foreach (var item in freq)
+            {
+                Console.WriteLine(item.Key + " = " + item.Value);
+            }
+        }
+    }
+}
