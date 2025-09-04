e79483d0a802a72d51625592fba0fa01003459fa
Manideep Shivanathula <mshivanathula@worksoft.com>
2025-09-04 16:45:51 +0530

    void

---
+++ Void Method/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Method
+{
+    internal class Program
+    {
+        static void GreetUser(string name)
+        {
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("Welcome " + name); // not necessary to repeat name
+        }
+
+        static void Main()
+        {
+            string user = "Neha";
+            GreetUser(user);
+        }
+    }
+}
