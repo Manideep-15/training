1edd3a3aa515b63f964581a69b2cf2ea0d5f643a
Manideep Shivanathula <mshivanathula@worksoft.com>
2025-09-04 16:48:29 +0530

    error method

---
+++ Division Error/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Division_Error
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 0;
+
+            try
+            {
+                int result = a / b;
+                Console.WriteLine("Result is: " + result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Can't divide by zero!");
+            }
+        }
+    }
+}
