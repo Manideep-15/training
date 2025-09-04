1821c0f5801c533a1202a79f08578eacd7705fb4
Manideep Shivanathula <mshivanathula@worksoft.com>
2025-09-04 17:13:50 +0530

    vote

---
+++ voting eligibility/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace voting_eligibility
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+          
+            if (age > 18)
+                Console.WriteLine("You can vote.");
+            else
+                Console.WriteLine("You cannot vote.");
+        }
+    }
+}
