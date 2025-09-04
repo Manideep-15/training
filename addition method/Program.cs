6f75ef6c2b1378906c8fe4999f9a253651534f67
Manideep Shivanathula <mshivanathula@worksoft.com>
2025-09-04 17:09:37 +0530

    addition

---
+++ addition method/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addition_method
+{
+    internal class Program
+    {
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void Main()
+        {
+            Console.Write("Enter first number: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Sum is: " + Add(x, y));
+        }
+    }
+}
