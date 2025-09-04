8a9f03470c6913fdbff197bc47d4a4ab1eb41070
Manideep Shivanathula <mshivanathula@worksoft.com>
2025-09-04 16:43:26 +0530

    method

---
+++ Method to Multiply Two Nums/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_to_Multiply_Two_Nums
+{
+    internal class Program
+    {
+        static int Multiply(int a, int b)
+        {
+            int result = a * b;
+            return result;
+        }
+
+        static void Main()
+        {
+            int x = 4;
+            int y = 5;
+            int res = Multiply(x, y);
+            Console.WriteLine("Multiplication is: " + res);
+        }
+    }
+}
