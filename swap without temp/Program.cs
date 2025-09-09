bc0937b99433061b94afbaa909ff5e48c7c00c1c
Manideep Shivanathula <mshivanathula@worksoft.com>
2025-09-09 12:34:38 +0530

    without temp

---
+++ swap without temp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swap_without_temp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your value: a = ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter your value: b = ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Before swap: a = " + a + " , b = " + b);
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine("After swap: a = " + a + " , b =5" +
+                "5 " + b);
+
+        }
+    }
+}
